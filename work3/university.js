bb3591a18d348d600b4d70a285be2ae64f40e8ea
Romik Sargsyan <sargsyanromik6@gmail.com>
2023-03-25 15:40:05 +0300

    changes

--- work3/university.js
+++ work3/university.js
@@ -4,35 +4,44 @@ class University{
         this.teachers = [],
         this.students = []       
     }
-    addMembers(member){
-
+    addMembers(member, role){
+        if(role === 'teacher'){
+            this.teachers.push(member)
+        }else if(role === 'student'){
+            this.students.push(member)
+        }
     }
     removeMember(member){
-
-    }
-    startLesson(){
-    
-
+        if(role === 'teacher'){
+            for(let i = 0; i < this.teachers.length; i++){
+                if(this.teachers[i] === member){
+                    delete this.teachers[i]
+                }
+            }        }
+         else if(role === 'student'){  
+            for(let i = 0; i < this.teachers.length; i++){
+                if(this.teachers[i] === member){
+                    delete this.students[i]
+                }               
+            }
+        }
     }
 }
 class UniversityMembers{
+    constructor(name, age, role){
+        this.name = name,
+        this.age = age,
+        this.role = role,
+        this.energy = 24
+    }
     info(){
         return `Name is ${this.name}, Age is ${this.age}, Role is ${this.role}, Enegry ${this.energy}`
     }
 }
 class Teacher extends UniversityMembers{
-    constructor(name, age, energy){
-        this.name = name,
-        this.age = age,
-        this.role = 'teacher',
-        this.energy = energy
-    }
+
 }
 class Student extends UniversityMembers{
-    constructor(name, age, energy){
-        this.name = name,
-        this.age = age,
-        this.role = 'student',
-        this.energy = energy
-    }
+    
 }
+
